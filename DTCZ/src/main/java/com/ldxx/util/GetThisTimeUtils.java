5b031bf270bae37bc59702c9fcd5a2a526d7682d
夏宁 <1042449477@qq.com>
2020-01-02 13:32:46 +0800

    xn

---
+++ DTCZ/src/main/java/com/ldxx/util/GetThisTimeUtils.java
@@ -0,0 +1,28 @@
+package com.ldxx.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GetThisTimeUtils {
+	
+	public static String getDateTime(){
+		Date date = new Date();
+		SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return dateFormat.format(date);
+	}
+	public static String getDate() {
+		Date date = new Date();
+		SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd");
+		return dateFormat.format(date);
+	}
+	public static String getTime() {
+		Date date = new Date();
+		SimpleDateFormat dateFormat= new SimpleDateFormat("HH:mm:ss");
+		return dateFormat.format(date);
+	}
+	public static String getDateTimeNumber(){
+		Date date = new Date();
+		SimpleDateFormat dateFormat= new SimpleDateFormat("yyyyMMddHHmmss");
+		return dateFormat.format(date);
+	}
+}
