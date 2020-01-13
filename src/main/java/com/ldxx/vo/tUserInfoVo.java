054cb4f15daca835eb5070188a5e3145c6ca2f9d
夏宁 <1042449477@qq.com>
2020-01-13 17:43:15 +0800

    xn

--- src/main/java/com/ldxx/vo/tUserInfoVo.java
+++ src/main/java/com/ldxx/vo/tUserInfoVo.java
@@ -4,22 +4,13 @@ import com.ldxx.bean.tUserInfo;
 
 public class tUserInfoVo extends tUserInfo {
 
-    private Double lng;
-    private Double lat;
+    private String lnglat;
 
-    public Double getLng() {
-        return lng;
+    public String getLnglat() {
+        return lnglat;
     }
 
-    public void setLng(Double lng) {
-        this.lng = lng;
-    }
-
-    public Double getLat() {
-        return lat;
-    }
-
-    public void setLat(Double lat) {
-        this.lat = lat;
+    public void setLnglat(String lnglat) {
+        this.lnglat = lnglat;
     }
 }
