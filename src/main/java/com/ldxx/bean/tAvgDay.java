d840e142c477e6d6ec66020f164059969c1621f2
lzf <1042142527@qq.com>
2020-02-17 18:22:42 +0800

    lzf

--- src/main/java/com/ldxx/bean/tAvgDay.java
+++ src/main/java/com/ldxx/bean/tAvgDay.java
@@ -8,10 +8,10 @@ public class tAvgDay {
     private int avgCount;
     private int axlesCount2;
     private int axlesCount3;
-    private int axles_Count4;
-    private int axles_Count5;
-    private int axles_Count6;
-    private int avg_Overload_Count;
+    private int axlesCount4;
+    private int axlesCount5;
+    private int axlesCount6;
+    private int avgOverloadCount;
     private int avgOverloadCount2;
     private int avgOverloadCount3;
     private int avgOverloadCount4;
@@ -25,187 +25,187 @@ public class tAvgDay {
     private double avgOverloadRatio6;
     private String stationIP;
 
-        public String getiD() {
-                return iD;
-        }
+    public String getiD() {
+        return iD;
+    }
 
-        public void setiD(String iD) {
-                this.iD = iD;
-        }
+    public void setiD(String iD) {
+        this.iD = iD;
+    }
 
-        public String getAvgTime() {
-                return avgTime;
-        }
+    public String getAvgTime() {
+        return avgTime;
+    }
 
-        public void setAvgTime(String avgTime) {
-                this.avgTime = avgTime;
-        }
+    public void setAvgTime(String avgTime) {
+        this.avgTime = avgTime;
+    }
 
-        public int getAvgLaneNo() {
-                return avgLaneNo;
-        }
+    public int getAvgLaneNo() {
+        return avgLaneNo;
+    }
 
-        public void setAvgLaneNo(int avgLaneNo) {
-                this.avgLaneNo = avgLaneNo;
-        }
+    public void setAvgLaneNo(int avgLaneNo) {
+        this.avgLaneNo = avgLaneNo;
+    }
 
-        public double getAvgMax() {
-                return avgMax;
-        }
+    public double getAvgMax() {
+        return avgMax;
+    }
 
-        public void setAvgMax(double avgMax) {
-                this.avgMax = avgMax;
-        }
+    public void setAvgMax(double avgMax) {
+        this.avgMax = avgMax;
+    }
 
-        public int getAvgCount() {
-                return avgCount;
-        }
+    public int getAvgCount() {
+        return avgCount;
+    }
 
-        public void setAvgCount(int avgCount) {
-                this.avgCount = avgCount;
-        }
+    public void setAvgCount(int avgCount) {
+        this.avgCount = avgCount;
+    }
 
-        public int getAxlesCount2() {
-                return axlesCount2;
-        }
+    public int getAxlesCount2() {
+        return axlesCount2;
+    }
 
-        public void setAxlesCount2(int axlesCount2) {
-                this.axlesCount2 = axlesCount2;
-        }
+    public void setAxlesCount2(int axlesCount2) {
+        this.axlesCount2 = axlesCount2;
+    }
 
-        public int getAxlesCount3() {
-                return axlesCount3;
-        }
+    public int getAxlesCount3() {
+        return axlesCount3;
+    }
 
-        public void setAxlesCount3(int axlesCount3) {
-                this.axlesCount3 = axlesCount3;
-        }
+    public void setAxlesCount3(int axlesCount3) {
+        this.axlesCount3 = axlesCount3;
+    }
 
-        public int getAxles_Count4() {
-                return axles_Count4;
-        }
+    public int getAxlesCount4() {
+        return axlesCount4;
+    }
 
-        public void setAxles_Count4(int axles_Count4) {
-                this.axles_Count4 = axles_Count4;
-        }
+    public void setAxlesCount4(int axlesCount4) {
+        this.axlesCount4 = axlesCount4;
+    }
 
-        public int getAxles_Count5() {
-                return axles_Count5;
-        }
+    public int getAxlesCount5() {
+        return axlesCount5;
+    }
 
-        public void setAxles_Count5(int axles_Count5) {
-                this.axles_Count5 = axles_Count5;
-        }
+    public void setAxlesCount5(int axlesCount5) {
+        this.axlesCount5 = axlesCount5;
+    }
 
-        public int getAxles_Count6() {
-                return axles_Count6;
-        }
+    public int getAxlesCount6() {
+        return axlesCount6;
+    }
 
-        public void setAxles_Count6(int axles_Count6) {
-                this.axles_Count6 = axles_Count6;
-        }
+    public void setAxlesCount6(int axlesCount6) {
+        this.axlesCount6 = axlesCount6;
+    }
 
-        public int getAvg_Overload_Count() {
-                return avg_Overload_Count;
-        }
+    public int getAvgOverloadCount() {
+        return avgOverloadCount;
+    }
 
-        public void setAvg_Overload_Count(int avg_Overload_Count) {
-                this.avg_Overload_Count = avg_Overload_Count;
-        }
+    public void setAvgOverloadCount(int avgOverloadCount) {
+        this.avgOverloadCount = avgOverloadCount;
+    }
 
-        public int getAvgOverloadCount2() {
-                return avgOverloadCount2;
-        }
+    public int getAvgOverloadCount2() {
+        return avgOverloadCount2;
+    }
 
-        public void setAvgOverloadCount2(int avgOverloadCount2) {
-                this.avgOverloadCount2 = avgOverloadCount2;
-        }
+    public void setAvgOverloadCount2(int avgOverloadCount2) {
+        this.avgOverloadCount2 = avgOverloadCount2;
+    }
 
-        public int getAvgOverloadCount3() {
-                return avgOverloadCount3;
-        }
+    public int getAvgOverloadCount3() {
+        return avgOverloadCount3;
+    }
 
-        public void setAvgOverloadCount3(int avgOverloadCount3) {
-                this.avgOverloadCount3 = avgOverloadCount3;
-        }
+    public void setAvgOverloadCount3(int avgOverloadCount3) {
+        this.avgOverloadCount3 = avgOverloadCount3;
+    }
 
-        public int getAvgOverloadCount4() {
-                return avgOverloadCount4;
-        }
+    public int getAvgOverloadCount4() {
+        return avgOverloadCount4;
+    }
 
-        public void setAvgOverloadCount4(int avgOverloadCount4) {
-                this.avgOverloadCount4 = avgOverloadCount4;
-        }
+    public void setAvgOverloadCount4(int avgOverloadCount4) {
+        this.avgOverloadCount4 = avgOverloadCount4;
+    }
 
-        public int getAvgOverloadCount5() {
-                return avgOverloadCount5;
-        }
+    public int getAvgOverloadCount5() {
+        return avgOverloadCount5;
+    }
 
-        public void setAvgOverloadCount5(int avgOverloadCount5) {
-                this.avgOverloadCount5 = avgOverloadCount5;
-        }
+    public void setAvgOverloadCount5(int avgOverloadCount5) {
+        this.avgOverloadCount5 = avgOverloadCount5;
+    }
 
-        public int getAvgOverloadCount6() {
-                return avgOverloadCount6;
-        }
+    public int getAvgOverloadCount6() {
+        return avgOverloadCount6;
+    }
 
-        public void setAvgOverloadCount6(int avgOverloadCount6) {
-                this.avgOverloadCount6 = avgOverloadCount6;
-        }
+    public void setAvgOverloadCount6(int avgOverloadCount6) {
+        this.avgOverloadCount6 = avgOverloadCount6;
+    }
 
-        public double getAvgOverloadRatio() {
-                return avgOverloadRatio;
-        }
+    public double getAvgOverloadRatio() {
+        return avgOverloadRatio;
+    }
 
-        public void setAvgOverloadRatio(double avgOverloadRatio) {
-                this.avgOverloadRatio = avgOverloadRatio;
-        }
+    public void setAvgOverloadRatio(double avgOverloadRatio) {
+        this.avgOverloadRatio = avgOverloadRatio;
+    }
 
-        public double getAvgOverloadRatio2() {
-                return avgOverloadRatio2;
-        }
+    public double getAvgOverloadRatio2() {
+        return avgOverloadRatio2;
+    }
 
-        public void setAvgOverloadRatio2(double avgOverloadRatio2) {
-                this.avgOverloadRatio2 = avgOverloadRatio2;
-        }
+    public void setAvgOverloadRatio2(double avgOverloadRatio2) {
+        this.avgOverloadRatio2 = avgOverloadRatio2;
+    }
 
-        public double getAvgOverloadRatio3() {
-                return avgOverloadRatio3;
-        }
+    public double getAvgOverloadRatio3() {
+        return avgOverloadRatio3;
+    }
 
-        public void setAvgOverloadRatio3(double avgOverloadRatio3) {
-                this.avgOverloadRatio3 = avgOverloadRatio3;
-        }
+    public void setAvgOverloadRatio3(double avgOverloadRatio3) {
+        this.avgOverloadRatio3 = avgOverloadRatio3;
+    }
 
-        public double getAvgOverloadRatio4() {
-                return avgOverloadRatio4;
-        }
+    public double getAvgOverloadRatio4() {
+        return avgOverloadRatio4;
+    }
 
-        public void setAvgOverloadRatio4(double avgOverloadRatio4) {
-                this.avgOverloadRatio4 = avgOverloadRatio4;
-        }
+    public void setAvgOverloadRatio4(double avgOverloadRatio4) {
+        this.avgOverloadRatio4 = avgOverloadRatio4;
+    }
 
-        public double getAvgOverload_Ratio5() {
-                return avgOverload_Ratio5;
-        }
+    public double getAvgOverload_Ratio5() {
+        return avgOverload_Ratio5;
+    }
 
-        public void setAvgOverload_Ratio5(double avgOverload_Ratio5) {
-                this.avgOverload_Ratio5 = avgOverload_Ratio5;
-        }
+    public void setAvgOverload_Ratio5(double avgOverload_Ratio5) {
+        this.avgOverload_Ratio5 = avgOverload_Ratio5;
+    }
 
-        public double getAvgOverloadRatio6() {
-                return avgOverloadRatio6;
-        }
+    public double getAvgOverloadRatio6() {
+        return avgOverloadRatio6;
+    }
 
-        public void setAvgOverloadRatio6(double avgOverloadRatio6) {
-                this.avgOverloadRatio6 = avgOverloadRatio6;
-        }
+    public void setAvgOverloadRatio6(double avgOverloadRatio6) {
+        this.avgOverloadRatio6 = avgOverloadRatio6;
+    }
 
-        public String getStationIP() {
-                return stationIP;
-        }
+    public String getStationIP() {
+        return stationIP;
+    }
 
-        public void setStationIP(String stationIP) {
-                this.stationIP = stationIP;
-        }
+    public void setStationIP(String stationIP) {
+        this.stationIP = stationIP;
+    }
 }
