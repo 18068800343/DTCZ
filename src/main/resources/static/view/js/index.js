a8d505b513a839a65145810069543f94c65dfdde
lzf <1042142527@qq.com>
2020-02-25 12:26:14 +0800

    lzf

--- src/main/resources/static/view/js/index.js
+++ src/main/resources/static/view/js/index.js
@@ -1,135 +1,92 @@
+myChart.showLoading();
 
-/*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+$.get(ROOT_PATH + 'data/asset/geo/HK.json', function (geoJson) {
 
-var _echarts = require("./lib/echarts");
+    myChart.hideLoading();
 
-(function () {
-  for (var key in _echarts) {
-    if (_echarts == null || !_echarts.hasOwnProperty(key) || key === 'default' || key === '__esModule') return;
-    exports[key] = _echarts[key];
-  }
-})();
+    echarts.registerMap('HK', geoJson);
 
-var _export = require("./lib/export");
-
-(function () {
-  for (var key in _export) {
-    if (_export == null || !_export.hasOwnProperty(key) || key === 'default' || key === '__esModule') return;
-    exports[key] = _export[key];
-  }
-})();
-
-require("./lib/component/dataset");
-
-require("./lib/chart/line");
-
-require("./lib/chart/bar");
-
-require("./lib/chart/pie");
-
-require("./lib/chart/scatter");
-
-require("./lib/chart/radar");
-
-require("./lib/chart/map");
-
-require("./lib/chart/tree");
-
-require("./lib/chart/treemap");
-
-require("./lib/chart/graph");
-
-require("./lib/chart/gauge");
-
-require("./lib/chart/funnel");
-
-require("./lib/chart/parallel");
-
-require("./lib/chart/sankey");
-
-require("./lib/chart/boxplot");
-
-require("./lib/chart/candlestick");
-
-require("./lib/chart/effectScatter");
-
-require("./lib/chart/lines");
-
-require("./lib/chart/heatmap");
-
-require("./lib/chart/pictorialBar");
-
-require("./lib/chart/themeRiver");
-
-require("./lib/chart/sunburst");
-
-require("./lib/chart/custom");
-
-require("./lib/component/grid");
-
-require("./lib/component/polar");
-
-require("./lib/component/geo");
-
-require("./lib/component/singleAxis");
-
-require("./lib/component/parallel");
-
-require("./lib/component/calendar");
-
-require("./lib/component/graphic");
-
-require("./lib/component/toolbox");
-
-require("./lib/component/tooltip");
-
-require("./lib/component/axisPointer");
-
-require("./lib/component/brush");
-
-require("./lib/component/title");
-
-require("./lib/component/timeline");
-
-require("./lib/component/markPoint");
-
-require("./lib/component/markLine");
-
-require("./lib/component/markArea");
-
-require("./lib/component/legendScroll");
-
-require("./lib/component/legend");
-
-require("./lib/component/dataZoom");
-
-require("./lib/component/dataZoomInside");
-
-require("./lib/component/dataZoomSlider");
-
-require("./lib/component/visualMap");
-
-require("./lib/component/visualMapContinuous");
-
-require("./lib/component/visualMapPiecewise");
-
-require("zrender/lib/vml/vml");
-
-require("zrender/lib/svg/svg");
+    myChart.setOption(option = {
+        title: {
+            text: '香港18区人口密度 （2011）',
+            subtext: '人口密度数据来自Wikipedia',
+            sublink: 'http://zh.wikipedia.org/wiki/%E9%A6%99%E6%B8%AF%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83#cite_note-12'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{b}<br/>{c} (p / km2)'
+        },
+        toolbox: {
+            show: true,
+            orient: 'vertical',
+            left: 'right',
+            top: 'center',
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        visualMap: {
+            min: 800,
+            max: 50000,
+            text: ['High', 'Low'],
+            realtime: false,
+            calculable: true,
+            inRange: {
+                color: ['lightskyblue', 'yellow', 'orangered']
+            }
+        },
+        series: [
+            {
+                name: '香港18区人口密度',
+                type: 'map',
+                mapType: 'HK', // 自定义扩展图表类型
+                label: {
+                    show: true
+                },
+                data: [
+                    {name: '中西区', value: 20057.34},
+                    {name: '湾仔', value: 15477.48},
+                    {name: '东区', value: 31686.1},
+                    {name: '南区', value: 6992.6},
+                    {name: '油尖旺', value: 44045.49},
+                    {name: '深水埗', value: 40689.64},
+                    {name: '九龙城', value: 37659.78},
+                    {name: '黄大仙', value: 45180.97},
+                    {name: '观塘', value: 55204.26},
+                    {name: '葵青', value: 21900.9},
+                    {name: '荃湾', value: 4918.26},
+                    {name: '屯门', value: 5881.84},
+                    {name: '元朗', value: 4178.01},
+                    {name: '北区', value: 2227.92},
+                    {name: '大埔', value: 2180.98},
+                    {name: '沙田', value: 9172.94},
+                    {name: '西贡', value: 3368},
+                    {name: '离岛', value: 806.98}
+                ],
+                // 自定义名称映射
+                nameMap: {
+                    'Central and Western': '中西区',
+                    'Eastern': '东区',
+                    'Islands': '离岛',
+                    'Kowloon City': '九龙城',
+                    'Kwai Tsing': '葵青',
+                    'Kwun Tong': '观塘',
+                    'North': '北区',
+                    'Sai Kung': '西贡',
+                    'Sha Tin': '沙田',
+                    'Sham Shui Po': '深水埗',
+                    'Southern': '南区',
+                    'Tai Po': '大埔',
+                    'Tsuen Wan': '荃湾',
+                    'Tuen Mun': '屯门',
+                    'Wan Chai': '湾仔',
+                    'Wong Tai Sin': '黄大仙',
+                    'Yau Tsim Mong': '油尖旺',
+                    'Yuen Long': '元朗'
+                }
+            }
+        ]
+    });
+});
