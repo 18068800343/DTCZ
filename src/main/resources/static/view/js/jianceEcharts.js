0917a3ae0cbfb1e514830ce536eb0ba4ac2d63b0
lzf <1042142527@qq.com>
2020-03-26 17:50:06 +0800

    lzf

--- src/main/resources/static/view/js/jianceEcharts.js
+++ src/main/resources/static/view/js/jianceEcharts.js
@@ -1,12 +1,5 @@
 function setjiancezhandianLeft(list){
-    var seriesLabel = {
-        normal: {
-            show: true,
-            textBorderColor: '#336666',
-            color:'black',
-            textBorderWidth: 1
-        }
-    }
+
     var option = {
         title: {
         },
@@ -29,18 +22,66 @@ function setjiancezhandianLeft(list){
             data: list.stationNames.split(","),
             axisLabel : {//坐标轴刻度标签的相关设置。
                 interval:0,
-                rotate:"45"
+                //rotate:"45"  //文字倾斜
+                formatter : function(params){ //文字换行
+                    var newParamsName = "";// 最终拼接成的字符串
+                    var paramsNameNumber = params.length;// 实际标签的个数
+                    var provideNumber = 4;// 每行能显示的字的个数
+                    var rowNumber = Math.ceil(paramsNameNumber / provideNumber);// 换行的话，需要显示几行，向上取整
+                    /**
+                     * 判断标签的个数是否大于规定的个数， 如果大于，则进行换行处理 如果不大于，即等于或小于，就返回原标签
+                     */
+                    // 条件等同于rowNumber>1
+                    if (paramsNameNumber > provideNumber) {
+                        /** 循环每一行,p表示行 */
+                        for (var p = 0; p < rowNumber; p++) {
+                            var tempStr = "";// 表示每一次截取的字符串
+                            var start = p * provideNumber;// 开始截取的位置
+                            var end = start + provideNumber;// 结束截取的位置
+                            // 此处特殊处理最后一行的索引值
+                            if (p == rowNumber - 1) {
+                                // 最后一次不换行
+                                tempStr = params.substring(start, paramsNameNumber);
+                            } else {
+                                // 每一次拼接字符串并换行
+                                tempStr = params.substring(start, end) + "\n";
+                            }
+                            newParamsName += tempStr;// 最终拼成的字符串
+                        }
+
+                    } else {
+                        // 将旧标签的值赋给新标签
+                        newParamsName = params;
+                    }
+                    //将最终的字符串返回
+                    return newParamsName
+                }
             }
         },
         yAxis: {
             type: 'value',
-            boundaryGap: [0, 0.01]
+            name: '单位(辆)',
+            position: 'left',
+            axisLine: {
+                lineStyle: {
+                    color: "#black"
+                }
+            },
+            axisLabel: {
+                formatter: '{value} '
+            }
         },
 
         series: [{
             name: '车流量',
             type: 'bar',
-            label: seriesLabel,
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top',
+                    color: 'black',
+                }
+            },
             data: list.nums.split(","),
             barWidth: 40,
             itemStyle: {
@@ -55,13 +96,7 @@ function setjiancezhandianLeft(list){
 }
 
 function setjiancezhandianRight(list){
-    var seriesLabel = {
-        normal: {
-            show: true,
-            textBorderColor: '#333',
-            textBorderWidth: 2
-        }
-    }
+
     var option = {
     	    tooltip: {
     	        trigger: 'axis',
@@ -87,13 +122,56 @@ function setjiancezhandianRight(list){
     	            data: list.stationNames.split(","),
                     axisLabel : {//坐标轴刻度标签的相关设置。
                         interval:0,
-                        rotate:"45"
-                    }
+                        //rotate:"45" //文字倾斜
+                        formatter : function(params){ //文字换行
+                            var newParamsName = "";// 最终拼接成的字符串
+                            var paramsNameNumber = params.length;// 实际标签的个数
+                            var provideNumber = 4;// 每行能显示的字的个数
+                            var rowNumber = Math.ceil(paramsNameNumber / provideNumber);// 换行的话，需要显示几行，向上取整
+                            /**
+                             * 判断标签的个数是否大于规定的个数， 如果大于，则进行换行处理 如果不大于，即等于或小于，就返回原标签
+                             */
+                            // 条件等同于rowNumber>1
+                            if (paramsNameNumber > provideNumber) {
+                                /** 循环每一行,p表示行 */
+                                for (var p = 0; p < rowNumber; p++) {
+                                    var tempStr = "";// 表示每一次截取的字符串
+                                    var start = p * provideNumber;// 开始截取的位置
+                                    var end = start + provideNumber;// 结束截取的位置
+                                    // 此处特殊处理最后一行的索引值
+                                    if (p == rowNumber - 1) {
+                                        // 最后一次不换行
+                                        tempStr = params.substring(start, paramsNameNumber);
+                                    } else {
+                                        // 每一次拼接字符串并换行
+                                        tempStr = params.substring(start, end) + "\n";
+                                    }
+                                    newParamsName += tempStr;// 最终拼成的字符串
+                                }
+
+                            } else {
+                                // 将旧标签的值赋给新标签
+                                newParamsName = params;
+                            }
+                            //将最终的字符串返回
+                            return newParamsName
+                        }
+                    },
     	        }
     	    ],
     	    yAxis: [
     	        {
-    	            type: 'value'
+    	            type: 'value',
+                    name: '单位(辆)',
+                    position: 'left',
+                    axisLine: {
+                        lineStyle: {
+                            color: "#black"
+                        }
+                    },
+                    axisLabel: {
+                        formatter: '{value} '
+                    }
     	        }
     	    ],
     	    series: [
@@ -101,6 +179,13 @@ function setjiancezhandianRight(list){
     	            name: '超载车辆',
     	            type: 'line',
     	            smooth:true,
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'top',
+                            color: 'black',
+                        }
+                    },
     	            stack: '总量',
     	            color:'#BCD8FE',
     	            areaStyle: {},
@@ -127,9 +212,10 @@ function setjiancezhandianBottom(list){
         },
 
         legend: {
-            data: ['二级预警(≥80吨)', '三级预警(≥100吨)'],
+            data: ['一级预警(≥80吨)', '二级预警(≥100吨)'],
             //right: 10,
             //orient: 'vertical',  //垂直显示
+            itemGap: 80,
             textStyle: {
                 color: '#17447E',
                 fontSize: 13
@@ -170,14 +256,24 @@ function setjiancezhandianBottom(list){
         ],
         yAxis: [
             {
-                show:false,
+                show:true,
                 type: 'value',
+                name: '单位(辆)',
+                position: 'left',
+                axisLine: {
+                    lineStyle: {
+                        color: "#black"
+                    }
+                },
+                axisLabel: {
+                    formatter: '{value} '
+                }
             },
 
         ],
         series: [
             {
-                name: '二级预警(≥80吨)',
+                name: '一级预警(≥80吨)',
                 type: 'bar',
                 label: {
                     normal: {
@@ -194,7 +290,7 @@ function setjiancezhandianBottom(list){
 
             },
             {
-                name: '三级预警(≥100吨)',
+                name: '二级预警(≥100吨)',
                 type: 'bar',
                 label: {
                     normal: {
