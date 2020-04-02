bd9576e7a44c5955d62eb430fa3429bdac978e42
lzf <1042142527@qq.com>
2020-04-02 09:18:25 +0800

    lzf

--- src/main/java/com/ldxx/util/DateJsonValueProcessor.java
+++ src/main/java/com/ldxx/util/DateJsonValueProcessor.java
@@ -10,7 +10,7 @@ import java.util.Date;
  *
  */
 public class DateJsonValueProcessor implements JsonValueProcessor {
-    private String format = "yyyy-MM-dd HH:mm:ss";
+    private String format = "yyyyMMddHHmmss";
 
     public DateJsonValueProcessor()
     {
