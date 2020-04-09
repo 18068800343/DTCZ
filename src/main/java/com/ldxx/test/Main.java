863a6849a92e8e6ff4ed1b7817552cb1b844321f
xianing <1042449477@qq.com>
2020-04-09 17:54:48 +0800

    xn

--- src/main/java/com/ldxx/test/Main.java
+++ src/main/java/com/ldxx/test/Main.java
@@ -1,82 +1,52 @@
 package com.ldxx.test;
 
-import java.util.Random;
-
-//消费者
-class Consumer extends Thread {
-    private Buffer buffer;
-
-    public Consumer(Buffer buffer) {
-        this.buffer = buffer;
-    }
-
-    @Override
-    public void run() {
-        int data;
-        while (true) {
-            data = buffer.consume();
-        }
-    }
-}
-
 public class Main {
+    public static String static_ma = "当前类静态全局变量";
+    public String normal_ma = "当前类全局变量";
+    public Main() {
+        System.out.println("当前主类构造方法");
+    }
+    {
+        System.out.println("当前类代码块");
+    }
+    static {
+        System.out.println("当前类静态代码块");
+    }
     public static void main(String[] args) {
-        final Buffer buffer = new Buffer();
-
+//       Main main = new Main();
+       Son father = new Son();
+    }
+}
+class Son extends Father{
+    public static String static_sa = "子类静态全局变量";
+    public String getStatic_sab = "子类全局变量";
+    {
+        System.out.println("子类代码块");
+    }
+    static {
+        System.out.println("子类静态代码块");
+    }
+    public Son() {
+        System.out.println("子类构造方法");
+    }
+    public static void SonStatic() {
+        System.out.println("子类静态方法");
     }
 }
 
-//生产者
-class Producer extends Thread {
-    private Buffer buffer;
-
-    public Producer(Buffer buffer) {
-        this.buffer = buffer;
+class Father{
+    public static String static_fa = "父类静态全局变量";
+    public String fa = "父类全局变量";
+    {
+        System.out.println("父类代码块");
     }
-
-    @Override
-    public void run() {
-        Random rand = new Random();
-        while (true) {
-            int n = rand.nextInt();
-            buffer.produce(n);
-        }
+   static {
+        System.out.println("父类静态代码块");
     }
-}
-
-class Buffer {
-    public int data;
-    public boolean empty;
-
-    public Buffer() {
-        this.empty = true;
+    public Father() {
+        System.out.println("父类构造方法");
     }
-
-    public synchronized void produce(int newData) {
-        while (!this.empty) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        this.data = newData;
-        this.empty = false;
-        this.notify();
-        System.out.println("Produced:" + newData);
-    }
-
-    public synchronized int consume() {
-        while (this.empty) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        this.empty = true;
-        this.notify();
-        System.out.println("Consumed:" + data);
-        return data;
+    public static void FatherStatic() {
+        System.out.println("父类静态方法");
     }
 }
