2ca0eb04976bb7b9d3bf7d9cceabd5f06cd6eb0e
xianing <1042449477@qq.com>
2020-08-13 15:05:04 +0800

    xn

--- src/main/java/com/ldxx/config/Const.java
+++ src/main/java/com/ldxx/config/Const.java
@@ -2,7 +2,7 @@ package com.ldxx.config;
 
 public interface Const {
     String CSV_HEAD = "站点,时间,车道号,车牌号,轴数,总重,轴1重,轴2重,轴3重,轴4重,轴5重,轴6重,轴7重,轴8重,轴9重,轴10重," +
-            "轴间距1,轴间距2,轴间距3,轴间距4,轴间距5,轴间距6,轴间距7,轴间距8,轴间距9,总轴距,车长,速度,是否跨道,温度,超载重量,超载比例";
+            "轴间距1,轴间距2,轴间距3,轴间距4,轴间距5,轴间距6,轴间距7,轴间距8,轴间距9,总轴距,车间距,车间时距,车头时距,车长,速度,是否跨道,温度,超载重量,超载比例";
     boolean REPORT_DAY = true;
     boolean REPORT_MONTH = true;
     boolean REPORT_WEEK = true;
